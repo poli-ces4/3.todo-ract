ba8ee205b413bf78e14d7fd87692335699957e43
Giovanny Vargas <yovaz05@gmail.com>
2023-09-19 19:56:52 -0500

    clase 19-09-2023

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import style from "./App.module.css";
 import Form from "./components/Form";
 import Header from "./components/Header";
 
 const App = () => {
+  const [inputTodo, setInputTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className={style.container}>
       <div className={style.appwrapper}>
@@ -10,7 +14,12 @@ const App = () => {
           <Header />
         </div>
         <div>
-          <Form />
+          <Form
+            inputTodo={inputTodo}
+            setInputTodo={setInputTodo}
+            todos={todos}
+            setTodos={setTodos}
+          />
         </div>
         <div>TodoList</div>
       </div>
