0e2fff09f4b2254e46bee247939592b680a7007d
Giovanny Vargas <yovaz05@gmail.com>
2023-09-19 19:48:46 -0500

    clase 19-09-2023

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import style from "./App.module.css";
+import Form from "./components/Form";
+import Header from "./components/Header";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className={style.container}>
+      <div className={style.appwrapper}>
+        <div>
+          <Header />
+        </div>
+        <div>
+          <Form />
+        </div>
+        <div>TodoList</div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
